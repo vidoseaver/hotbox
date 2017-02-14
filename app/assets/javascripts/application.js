b1ab7153623b55ee567846f2977a74fe2facf467
Vido <vidoseaver@gmail.com>
2017-02-13 17:32:19 -0700

    make it secure with requests to https

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require_tree .
 
 function getAllLinks(){
-   axios.get('http://vido-urlockbox.herokuapp.com/api/v1/links')
+   axios.get('https://vido-urlockbox.herokuapp.com/api/v1/links')
   .then(function (response) {
     renderAllLinksToPage(response.data)
   })
